f5eed22c1fbe526480f145efdb0bfa508b4e00cf
Felippe Matheo <69490109+matheofelippe@users.noreply.github.com>
2020-12-02 22:34:20 -0300

    Códigos

---
+++ Produto.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+
+public class Produto
+{
+    private string _nome; //variavel do nome do produto
+    private double _preco; //variavel do preço do produto
+    private int _quantidade; //variavel da quantidade de produtos
+
+    public Produto(string nome, double preco, int quantidade) //Construtor personalizado
+    {
+        _nome = nome;
+        _preco = preco;
+        _quantidade = quantidade;
+    }
+
+    public string Nome //Utilização do Properties
+    {
+        get { return _nome; }
+        set { if(value!=null && value.Length > 1)
+            {
+                _nome = value;
+            } 
+        }
+    }
+
+    public double Preco //Utilização do Properties para preço
+    {
+        get { return _preco; }
+    }
+
+    public int Quantidade //Utilização do Properties para quantidade
+    {
+        get { return _quantidade; }
+    }
+
+    public double ValorTotalEmEstoque() //metodo que mostra o valor total em reais do estoque
+    {
+        return _preco * _quantidade;
+    }
+
+    public void AdcionarProdutos(int quantidade) //metodo que adciona produtos no estoque
+    {
+        _quantidade = _quantidade + quantidade;
+    }
+
+    public void RemoverProdutos(int quantidade) //metodo que remove produtos do estoque
+    {
+        _quantidade = _quantidade - quantidade;
+    }
+
+    public override string ToString() //metodo que mostra todos os dados do produtos
+    {
+        return _nome
+        + ", R$"
+        + _preco.ToString("F2", CultureInfo.InvariantCulture) //mostra apenas 2(F2) numeros depois da virgula
+        + ", "
+        + _quantidade
+        + " unidades, Total: R$"
+        + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture); //mostra apenas 2(F2) numeros depois da virgula
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_individual_Tema_Encapsulamento_e_Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("PS5", 2000.00, 5); //instancia um objeto passando paramentros ja na sua instanciação
+
+            p.Nome = "3DS"; // muda o nome do produto para 3DS
+
+            Console.WriteLine(p.Nome); //mostra no console o nome do objeto
+            Console.WriteLine(p.Preco); //mostra no console o preco do objeto
+            Console.WriteLine(p.Quantidade); //mostra no console a quantidade do objeto
+
+            Console.ReadKey();
+        }
+    }
+}
